7a4169c9976d4733a4846d6e2ae475c7563195ff
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-12 23:46:56 +0300

    Распределение по папкам

--- CourseWork_Algorithms_Data Structures/Program.cs
+++ CourseWork_Algorithms_Data Structures/Program.cs
@@ -6,7 +6,30 @@ namespace CourseWork_Algorithms_Data_Structures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Airport airport = new Airport("Kazan");
+
+            Airplane airplane1 = new Airplane("BRE", 1956);
+            Airplane airplane2 = new Airplane("DVC", 2000);
+            Airplane airplane3 = new Airplane("UIR", 2005);
+
+            airport.Push(airplane1);
+            airport.Push(airplane2);
+            airport.Push(airplane3);
+
+            Airport airport2 = new Airport("Moscow");
+
+            Airplane airplane6 = new Airplane("YRE", 2001);
+            Airplane airplane4 = new Airplane("KLI", 2007);
+            Airplane airplane5 = new Airplane("PWQ", 2008);
+
+            airport2.Push(airplane4);
+            airport2.Push(airplane5);
+            airport2.Push(airplane6);
+
+            AirCompany company = new AirCompany("Победа");
+
+            company.Push(airport);
+            company.Push(airport2);
         }
     }
 }
