61da1c214382180a8f796bd858183561b777e517
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-12 22:29:46 +0300

    Создал класс самолета

---
+++ CourseWork_Algorithms_Data Structures/Airplane.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork_Algorithms_Data_Structures
+{
+    public class Airplane
+    {
+        public string Brand { get; private set; }
+
+        public string YearofManufacture { get; private set; }
+
+        public Airplane(string Brand, string YearofManufacture)
+        {
+            this.Brand = Brand;
+            this.YearofManufacture = YearofManufacture;
+        }
+    }
+}
