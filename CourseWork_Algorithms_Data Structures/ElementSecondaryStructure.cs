9f1192a67478c8c302df450e16b60cdfe7e81466
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-12 22:38:40 +0300

    Класс ячейки второстепенной структуры

---
+++ CourseWork_Algorithms_Data Structures/ElementSecondaryStructure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork_Algorithms_Data_Structures
+{
+    public class ElementSecondaryStructure
+    {
+        public Airplane Airplane { get; private set; }
+
+        public ElementSecondaryStructure Next { get; private set; }
+
+        public ElementSecondaryStructure(Airplane airplane)
+        {
+            Airplane = airplane;
+        }
+    }
+}
