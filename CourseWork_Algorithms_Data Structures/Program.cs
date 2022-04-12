4a4a11d528354549a578ab3dcc6ec70ae8e1c90f
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-12 22:21:55 +0300

    Добавьте файлы проекта.

---
+++ CourseWork_Algorithms_Data Structures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CourseWork_Algorithms_Data_Structures
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
