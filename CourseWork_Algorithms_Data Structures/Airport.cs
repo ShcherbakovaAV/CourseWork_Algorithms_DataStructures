f7af3167b23794c103caa264fbb46c1a2c01aabd
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-12 23:09:01 +0300

    Описание класса - Аэропорт

---
+++ CourseWork_Algorithms_Data Structures/Airport.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork_Algorithms_Data_Structures
+{
+    public class Airport
+    {
+        public string Name { get; private set; }
+
+        private ElementSecondaryStructure _head;
+
+        public int Count { get; private set; }
+
+        public bool IsEmpty => Count == 0;
+
+        public Airport(string name)
+        {
+            Name = name;
+        }
+
+        public void Push(Airplane airplane)
+        {
+            if (airplane is null)
+                throw new ArgumentNullException(nameof(airplane));
+
+            ElementSecondaryStructure node = new ElementSecondaryStructure(airplane);
+
+            node.Next = _head;
+            _head = node;
+
+            Count++;
+        }
+
+        public void Push(string brand, string year)
+        {
+            if (brand is null || year is null)
+                throw new ArgumentNullException();
+
+            Airplane airplane = new Airplane(brand, year);
+            Push(airplane);
+        }
+
+        public Airplane Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException();
+
+            Airplane result = _head.Airplane;
+            _head = _head.Next;
+
+            Count--;
+            return result;
+        }
+
+        public Airplane Peek()
+        {
+            return _head.Airplane;
+        }
+
+        public bool Contains(Airplane airplane)
+        {
+            ElementSecondaryStructure _current = _head;
+
+            while (_current != null)
+            {
+                if (_current.Airplane == airplane)
+                    return true;
+
+                _current = _current.Next;
+            }
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            _head = null;
+        }
+    }
+}
--- CourseWork_Algorithms_Data Structures/ElementSecondaryStructure.cs
+++ CourseWork_Algorithms_Data Structures/ElementSecondaryStructure.cs
@@ -10,7 +10,7 @@ namespace CourseWork_Algorithms_Data_Structures
     {
         public Airplane Airplane { get; private set; }
 
-        public ElementSecondaryStructure Next { get; private set; }
+        public ElementSecondaryStructure Next { get; set; }
 
         public ElementSecondaryStructure(Airplane airplane)
         {
