99347c1948d27259a4a7bf3f00d3a31a388e4b1f
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-12 23:43:40 +0300

    Описание класса элемента главной структуры

---
+++ CourseWork_Algorithms_Data Structures/ElementMainStructure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork_Algorithms_Data_Structures
+{
+    public class ElementMainStructure
+    {
+        public ElementMainStructure Next { get; set; }
+
+        public Airport Airport { get; set; }
+
+        public ElementMainStructure(Airport airport)
+        {
+            Airport = airport;
+        }
+    }
+}
