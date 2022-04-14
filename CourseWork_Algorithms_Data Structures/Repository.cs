a83fe4ec7551e4201fc6aac4379bf07800502138
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-14 20:56:44 +0300

    Заготовка класса, для работы с XML

---
+++ CourseWork_Algorithms_Data Structures/Repository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork_Algorithms_Data_Structures
+{
+    /// <summary>
+    /// Класс, предназначенный для работы с xml
+    /// </summary>
+    public class Repository
+    {
+        public void SaveToXml(string file_path, AirCompany company)
+        {
+
+        }
+
+        public AirCompany DownloadFromXml(string file_path)
+        {
+
+        }
+    }
+}
