e8da9c58985b73f273fec116e192f227ac241682
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-23 14:27:26 +0300

    Создание класса конфигураций, который будет программно хранить настройки

---
+++ CourseWork_Algorithms_Data Structures/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+
+namespace CourseWork_Algorithms_Data_Structures
+{
+    public class Configuration
+    {
+        public string FileInput { get; set; }
+
+        public string FileOutput { get; set; }
+
+    }
+}
