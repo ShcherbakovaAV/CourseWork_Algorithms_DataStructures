4d659557700161f420baf8f3e24b1e04a53972b0
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-23 15:02:12 +0300

    Вынос логики из класса - Репозиторий в наш сервис

--- CourseWork_Algorithms_Data Structures/Repository.cs
+++ CourseWork_Algorithms_Data Structures/Repository.cs
@@ -41,42 +41,7 @@ namespace CourseWork_Algorithms_Data_Structures
         /// <param name="company"></param>
         public void SaveToXml(AirCompany company, string file_path = null)
         {
-            XDocument xdoc = new XDocument();
-
-            XElement company_ = new XElement("company");
-
-            XAttribute companyNameAttr = new XAttribute("name", company.Name);
-
-            company_.Add(companyNameAttr);
-
-            foreach (var airport in company)
-            {
-                XElement airport_ = new XElement("airport");
-
-                XAttribute airportNameAttr = new XAttribute("name", airport.Name);
-
-                airport_.Add(airportNameAttr);
-
-                foreach (var airplane in airport)
-                {
-                    XElement airplane_ = new XElement("airplane");
-                    XElement airplane_brand = new XElement("brand", airplane.Brand);
-                    XElement airplane_year = new XElement("year", airplane.YearofManufacture);
-                    airplane_.Add(airplane_brand);
-                    airplane_.Add(airplane_year);
-                    airport_.Add(airplane_);
-                }
-
-                company_.Add(airport_);
-            }
-
-            xdoc.Add(company_);
-
-            //сохраняем документ
-            if (file_path is null)
-                xdoc.Save(_filePathOutput);
-            else
-                xdoc.Save(file_path);
+            
         }
 
         /// <summary>
@@ -86,51 +51,7 @@ namespace CourseWork_Algorithms_Data_Structures
         /// <returns></returns>
         public AirCompany DownloadFromXml(string file_path = null)
         {
-            XmlDocument xDoc = new XmlDocument();
-
-            if (file_path is null)
-                xDoc.Load(_filePathInput);
-            else
-                xDoc.Load(file_path);
-
-            XmlElement xRoot = xDoc.DocumentElement;
-
-            AirCompany company = new AirCompany(xRoot.GetAttribute("name").ToString());
-
-            if (xRoot != null)
-            {
-                foreach (XmlElement xnode in xRoot)
-                {
-                    string name_airport = xnode.Attributes.GetNamedItem("name").Value;
-                    Airport airport = new Airport(name_airport);
-                    company.PushAirport(airport);
-
-                    foreach (XmlNode childnode in xnode.ChildNodes)
-                    {
-                        string airplane_name = null;
-                        string airplane_year = null;
-
-                        foreach (XmlNode ch in childnode.ChildNodes)
-                        {
-                            if (ch.Name == "brand")
-                            {
-                                airplane_name = ch.InnerText;
-                            }
-
-                            if (ch.Name == "year")
-                            {
-                                airplane_year = ch.InnerText;
-                            }
-                        }
-                        
-                        Airplane airplane = new Airplane(airplane_name, Convert.ToInt32(airplane_year));
-
-                        airport.Push(airplane);
-                    }
-                }
-            }
-
-            return company;
+            
         }
     }
 }
---
+++ CourseWork_Algorithms_Data Structures/Services/Interfaces/IXmlService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork_Algorithms_Data_Structures.Services.Interfaces
+{
+    public interface IXmlService
+    {
+        void SaveToXml(AirCompany company, string file_path);
+
+        AirCompany DownloadFromXml(string file_path);
+    }
+}
