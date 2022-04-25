ef5c52a315ef01d32b6108eb8cd0309cb0611ebd
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-25 21:27:46 +0300

    Распределение по папкам

---
+++ CourseWork.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CourseWork.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var service_collection = new ServiceCollection();
+            service_collection.AddSingleton<IWorkingFileService, XmlService>();
+
+            //Получение сервисов
+            var provider = service_collection.BuildServiceProvider();
+            var service = provider.GetRequiredService<IWorkingFileService>();
+
+            //Создание объекта - Репозиторий, главный класс, для работы со структурой
+            Repository rep = new Repository(service);
+
+            ConsoleApp.Run(rep);
+        }
+    }
+}
--- CourseWork_Algorithms_Data Structures/Program.cs
+++ CourseWork_Algorithms_Data Structures/Program.cs
@@ -10,17 +10,7 @@ namespace CourseWork_Algorithms_Data_Structures
         static void Main(string[] args)
         {
             //Регистрация сервисов
-            var service_collection = new ServiceCollection();
-            service_collection.AddSingleton<IWorkingFileService, XmlService>();
-
-            //Получение сервисов
-            var provider = service_collection.BuildServiceProvider();
-            var service = provider.GetRequiredService<IWorkingFileService>();
-
-            //Создание объекта - Репозиторий, главный класс, для работы со структурой
-            Repository rep = new Repository(service);
-
-            ConsoleApp.Run(rep);
+            
         }
     }
 }
