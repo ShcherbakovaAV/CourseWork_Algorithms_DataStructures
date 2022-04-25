058c5d8dbbd5f35a48ebe7516aade73baa3f9324
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-25 22:53:23 +0300

    Заготовка сервиса для работы с Json-файлами

---
+++ Services/CourseWork.Services/JsonService.cs
@@ -0,0 +1,23 @@
+﻿using CourseWork_Algorithms_Data_Structures;
+using CourseWork_Algorithms_Data_Structures.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork.Services
+{
+    public class JsonService : IWorkingFileService
+    {
+        public AirCompany Download(string file_path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(AirCompany company, string file_path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
