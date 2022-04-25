b27df3851a8634ba30842743e4716b6f2b62e2ca
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-25 23:11:41 +0300

    Логика сервиса для работы с Json-файлами

--- Services/CourseWork.Services/JsonService.cs
+++ Services/CourseWork.Services/JsonService.cs
@@ -1,10 +1,10 @@
-﻿using CourseWork_Algorithms_Data_Structures;
+﻿using CourseWork.Services.JsonModels;
+using CourseWork_Algorithms_Data_Structures;
 using CourseWork_Algorithms_Data_Structures.Services.Interfaces;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace CourseWork.Services
 {
@@ -12,12 +12,61 @@ namespace CourseWork.Services
     {
         public AirCompany Download(string file_path)
         {
-            throw new NotImplementedException();
+            var main_object = JsonConvert.DeserializeObject<ObjMainJson>(file_path);
+
+            AirCompany company = new AirCompany(main_object.Name);
+
+            foreach (var _airport in main_object.Airports)
+            {
+                Airport airport = new Airport(_airport.Name);
+
+                company.PushAirport(airport);
+
+                foreach (var _airplane in _airport.Airplanes)
+                {
+                    string airplane_brand = _airplane.Brand;
+                    int airplane_year = _airplane.YearofManufacture;
+
+                    Airplane airplane = new Airplane(airplane_brand, airplane_year);
+
+                    airport.Push(airplane);
+                }
+            }
+
+            return company;
+
         }
 
         public void Save(AirCompany company, string file_path)
         {
-            throw new NotImplementedException();
+            var main_object = new ObjMainJson()
+            {
+                Name = company.Name,
+                Airports = new List<ObjSecondJson>()
+            };
+
+            foreach (var _airport in company)
+            {
+                var airport = new ObjSecondJson()
+                {
+                    Name = _airport.Name,
+                    Airplanes = new List<Airplane>()
+                };
+
+                foreach (var _airplane in _airport)
+                {
+                    var airplane = new Airplane(_airplane.Brand, _airplane.YearofManufacture);
+                    airport.Airplanes.Add(airplane);
+                }
+
+                main_object.Airports.Add(airport);
+            }
+
+            string result = JsonConvert.SerializeObject(main_object);
+
+            StreamWriter file = File.CreateText(file_path);
+            file.WriteLine(result);
+            file.Close();
         }
     }
 }
