a511842b4af35595c617b5e71f166374116d9e68
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-25 23:06:28 +0300

    Добавление классов для сериализации-десериализации

---
+++ Services/CourseWork.Services/JsonModels/ObjMainJson.cs
@@ -0,0 +1,19 @@
+﻿using CourseWork_Algorithms_Data_Structures;
+using System.Collections.Generic;
+
+namespace CourseWork.Services.JsonModels
+{
+    public class ObjMainJson
+    {
+        public string Name { get; set; }
+
+        public List<ObjSecondJson> Airports { get; set; }
+    }
+
+    public class ObjSecondJson
+    {
+        public string Name { get; set; }
+
+        public List<Airplane> Airplanes { get; set; }
+    }
+}
