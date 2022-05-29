586128d63d5571ce4c367cdb4ff4cc950fba526b
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-05-29 17:18:15 +0300

    Создал ViewModel

---
+++ UI/CourseWork.WebApp/Models/DataInputViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseWork.WebApp.Models
+{
+    public class DataInputViewModel
+    {
+        public string? FileOutput { get; set; }
+
+        public string? FileType { get; set; }
+    }
+}
