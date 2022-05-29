ddd3ee5b318ff50642a8832afb0ec0135d265265
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-05-29 18:40:40 +0300

    Метод контроллера, отвечающий за вывод информации

---
+++ UI/CourseWork.WebApp/Controllers/AircompanyController.cs
@@ -0,0 +1,49 @@
+﻿using CourseWork.Services.Interfaces;
+using CourseWork.WebApp.Mapping;
+using CourseWork.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseWork.WebApp.Controllers
+{
+    public class AircompanyController : Controller
+    {
+        private readonly IStorage _storage;
+
+        public AircompanyController(IStorage storage)
+        {
+            _storage = storage;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new DataInputViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Loading(DataInputViewModel Model)
+        {
+            if (!ModelState.IsValid)
+                return View(Model);
+
+            if (!(Model.FileType == "json" || Model.FileType == "xml"))
+                ModelState.AddModelError("", "Некорректный тип файла");
+
+            if (Model.FileType == "json")
+                _storage.DownloadFromJson(Model.FileOutput);
+            else
+                _storage.DownloadFromXml(Model.FileOutput);
+
+            
+
+            return RedirectToAction(nameof(ShowCompany));
+        }
+
+        [HttpGet]
+        public IActionResult ShowCompany()
+        {
+            var aircompany_view = _storage.GetMainStructure().ToView();
+            return View(aircompany_view);
+        }
+    }
+}
