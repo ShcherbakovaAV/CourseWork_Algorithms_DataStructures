592be75a272cc9dcd7954302ff8f06bfcfb45482
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-05-29 18:06:29 +0300

    Создание маппера для аэрокомпании

---
+++ UI/CourseWork.WebApp/Mapping/AircompanyMapper.cs
@@ -0,0 +1,42 @@
+﻿using CourseWork.Structures.Structure;
+using CourseWork.WebApp.Models;
+
+namespace CourseWork.WebApp.Mapping
+{
+    public static class AircompanyMapper
+    {
+        public static AircompanyViewModel? ToView(this AirCompany? aircompany)
+        {
+            if (aircompany == null)
+                return null;
+
+            var viewModel = new AircompanyViewModel 
+            { 
+                Name = aircompany.Name,
+                CountAirpotrs = aircompany.CountAirport,
+                CountAirpanes = aircompany.CountAirplane
+            };
+
+            foreach (var airport in aircompany)
+            {
+                var airport_view = new AirportViewModel
+                {
+                    Name = airport.Name,
+                    CountAirplane = airport.Count
+                };
+                foreach (var airplane in airport)
+                {
+                    var airplane_view = new AirplaneViewModel
+                    {
+                        Brand = airplane.Brand,
+                        YearofManufacture = airplane.YearofManufacture,
+                    };
+                    airport_view.Airplanes.Push(airplane_view);
+                }
+                viewModel.Airports.Enqueue(airport_view);                
+            }
+
+            return viewModel;
+        }
+    }
+}
