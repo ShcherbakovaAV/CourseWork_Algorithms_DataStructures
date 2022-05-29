7f59dfb226a94701a1fb5999e8a9131b88b156d4
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-05-29 17:18:32 +0300

    Регистрация сервиса для работы с аэрокомпанией

--- UI/CourseWork.WebApp/Startup.cs
+++ UI/CourseWork.WebApp/Startup.cs
@@ -1,3 +1,5 @@
+using CourseWork.Repository;
+using CourseWork.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,13 +21,12 @@ namespace CourseWork.WebApp
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<IStorage, Storage>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
